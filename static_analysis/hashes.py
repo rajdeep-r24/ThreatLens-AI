83e7840b4970ef2391bcb24372bc7e3673f4cccb
reetushetty <reetushetty25ci@rnsit.ac.in>
2026-08-28 23:44:43 +0530

    Add MD5 and SHA-256 hashing

---
+++ static_analysis/hashes.py
@@ -0,0 +1,17 @@
+import hashlib
+
+def calculate_hashes(file_path):
+  md5 = hashlib.md5()
+  sha256 = hashlib.sha256()
+
+  with open(file_path, "rb") as file:
+    while chunk := file.read(8192):
+      md5.update(chunk)
+      sha256.update(chunk)
+
+  return {
+      "md5": md5.hexdigest()
+      "sha256": sha256.hexdigest()
+  }
+      
+      
