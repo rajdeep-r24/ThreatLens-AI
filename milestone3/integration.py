eafba148b3d944445dd8526dd93c0de42c46f324
medukundanagesh-alt <medukundanagesh@gmail.com>
2026-09-14 18:19:59 +0530

    Update integration.py

--- milestone3/integration.py
+++ milestone3/integration.py
@@ -1,1 +1,1 @@
-feat(member5): add milestone 3 integration and security report
+from app.api.v1.integration import router as integration_routerfeat(member5): add milestone 3 integration and security report
